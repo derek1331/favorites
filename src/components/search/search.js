bee61a8368d8b0331a799ac1b83327b5cd02f657
derek <1331derek@gmail.com>
2019-02-24 19:12:02 -0700

    the movie db

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -12,6 +12,7 @@ class Search extends React.Component {
         rated: '',
         actors: '',
         plot: '',
+        id: ''
     }
 
     enterFunction = (e) => {
@@ -25,28 +26,25 @@ class Search extends React.Component {
     searchFunction = () => {
         const inputSearch = document.getElementById('search-input')
         const value = inputSearch.value
-        fetch(`http://www.omdbapi.com/?t=${value}&apikey=447f330`)
+        fetch(`https://api.themoviedb.org/3/search/movie?api_key=92e3009a273b724b7b23754ceabd0fac&language=en-US&query=${value}&page=1&include_adult=false&`)
             .then(res => res.json())
-            .then(res => {
-                this.displayFunction(res)
-                console.log(res)
-            })
-            .catch(error => {
-                console.log(error)
-            })
-
+            .then(res => fetch(`https://api.themoviedb.org/3/movie/${res.results[0].id}?api_key=92e3009a273b724b7b23754ceabd0fac&language=en-US`))
+            .then(res => res.json())
+            .then(res => this.displayFunction(res))
     }
 
     displayFunction = (apiData) => {
-        const image = apiData.Poster;
+        const image = `https://image.tmdb.org/t/p/w220_and_h330_face/${apiData.poster_path}`;
         const link = apiData.Website;
-        const title = apiData.Title;
-        const year = apiData.Year;
+        const title = apiData.title;
+        const year = apiData.release_date;
         const rated = apiData.Rated;
         const actors = apiData.Actors;
-        const plot = apiData.Plot;
+        const plot = apiData.overview;
         // const link = apiData.Website;
         const noLink = document.getElementById('search-span')
+        console.log(apiData);
+
         if (apiData.Response === 'False') {
             alert('no title fix me pls')
         } else {
@@ -72,27 +70,56 @@ class Search extends React.Component {
 
     render() {
 
-        return (
-            <div id='search' className='search'>
-                <input type='text' className='search-input' id='search-input' onKeyDown={this.enterFunction}>
-                </input>
-                <button className='search-button' id='search-button' onClick={this.searchFunction}>Search</button>
-                <hr />
-                <div className='search-results' id='search-results'>
-                    <div id='search-results-container1' className='search-results-container1'>
-                        <div><span className='important-word'>Title: </span><span id='search-container1-info1' className='search-container1-info'>{this.state.title}</span></div>
-                        <div><span className='important-word'>Year: </span><span id='search-container1-info2' className='search-container1-info'>{this.state.year}</span></div>
-                        <div><span className='important-word'>Rated: </span><span id='search-container1-info3' className='search-container1-info'>{this.state.rated}</span></div>
-                        <div><span className='important-word'>Actors: </span><span id='search-container1-info3' className='search-container1-info'>{this.state.actors}</span></div>
-                        <div><span className='important-word'>Plot: </span><span id='search-container1-info3' className='search-container1-info'>{this.state.plot}</span></div>
-                    </div>
-                    <div id='search-results-container2' className='search-results-container2'>
-                        <a id='search-link' className='search-link' href={this.state.link}><img className='search-image' id='search-image' src={this.state.image}></img></a>
-                        <span hidden id='search-span'>No link Available</span>
-                    </div>
+        return ( <
+            div id = 'search'
+            className = 'search' >
+            <
+            input type = 'text'
+            className = 'search-input'
+            id = 'search-input'
+            onKeyDown = {
+                this.enterFunction
+            } >
+            <
+            /input> <
+            button className = 'search-button'
+            id = 'search-button'
+            onClick = {
+                this.searchFunction
+            } > Search < /button> <
+            hr / >
+            <
+            div className = 'search-results'
+            id = 'search-results' >
+            <
+            div id = 'search-results-container1'
+            className = 'search-results-container1' >
+            <
+            div > < span className = 'important-word' > Title: < /span><span id='search-container1-info1' className='search-container1-info'>{this.state.title}</span > < /div> <
+            div > < span className = 'important-word' > Year: < /span><span id='search-container1-info2' className='search-container1-info'>{this.state.year}</span > < /div> <
+            div > < span className = 'important-word' > Rated: < /span><span id='search-container1-info3' className='search-container1-info'>{this.state.rated}</span > < /div> <
+            div > < span className = 'important-word' > Actors: < /span><span id='search-container1-info4' className='search-container1-info'>{this.state.actors}</span > < /div> <
+            div > < span className = 'important-word' > Plot: < /span><span id='search-container1-info5' className='search-container1-info'>{this.state.plot}</span > < /div> <
+            /div> <
+            div id = 'search-results-container2'
+            className = 'search-results-container2' >
+            <
+            a id = 'search-link'
+            className = 'search-link'
+            href = {
+                this.state.link
+            } > < img className = 'search-image'
+            id = 'search-image'
+            src = {
+                this.state.image
+            } > < /img></a >
+            <
+            span hidden id = 'search-span' > No link Available < /span> <
+            /div>
 
-                </div>
-            </div>
+            <
+            /div> <
+            /div>
         )
     }
 }
